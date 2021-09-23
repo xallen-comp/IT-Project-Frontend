f686c644c74882058df8b0c7cf87721fe642acd4
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-09-23 17:52:28 +1000

    Comp-101-update to contact page

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -4,36 +4,46 @@ import axios from "../services/backendApi.js";
 
 const ContactPage = () => {
     const [items, setItems] = useState([]);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [comment, setComment] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     const [events, setEvents] = useState([]);
+    const [emailCheck, setEmailCheck] = useState(false);
     const GetContacts = () =>{
             axios.get("/contacts").then(res => {setItems(res.data);})
-    }
+    };
+    const onChangeEmail = (e) => {
+        const email = e.target.value;
+        setEmail(email);
+        checkEmailFormat(email)
+    };
     const GetEvents = () =>{
         axios.get("/events").then(res => {setEvents(res.data);})
     }
     useEffect(() => {
         GetContacts();
         GetEvents();
-    }, [])
+    }, []);
+
+    const validateUpdate = () => {
+        return emailCheck
+    }
+    const handleUpdate = (e) => {
+    }
+    //from webinfo 2021
+    const checkEmailFormat = (emailToCheck)=>{
+        console.log(emailToCheck)
+        const validateEmail = (emailToCheck) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(emailToCheck).toLowerCase());
+        }
+        setEmailCheck(validateEmail(emailToCheck))
+    
+    }
     return (
     <header className = "App-header">
-        <div>
-            {items.map((item, key) => (
-                <div>
-                <p>{item.first_name} </p>
-                <p>{item.last_name}</p>
-                <b>{item.comments}</b>
-                </div>
-            ))}
-            {events.map((event, key) => (
-                <div>
-                <p>{event.description} </p>
-                <p>{event.start}</p>
-                <b>{event.end}</b>
-                <b>{event.importance}</b>
-                </div>
-            ))}
-        </div>
    </header>
    );
 }
