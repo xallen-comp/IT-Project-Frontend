266590a26d2130fa29e98a8c898bba9ec2d1dbb7
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-09-23 18:26:03 +1000

    Comp-101:feat submit form

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -31,10 +31,16 @@ const ContactPage = () => {
         return emailCheck
     }
     const handleUpdate = (e) => {
+        console.log(e)
+        e.preventDefault();
+         if (true){
+            console.log(validateUpdate);
+         axios.post("contacts/add", 
+              {email: email, firstName: firstName, lastName: lastName, comments: comment}).then(res=> console.log(res))
+         }
     }
     //from webinfo 2021
     const checkEmailFormat = (emailToCheck)=>{
-        console.log(emailToCheck)
         const validateEmail = (emailToCheck) => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(emailToCheck).toLowerCase());
@@ -44,6 +50,63 @@ const ContactPage = () => {
     }
     return (
     <header className = "App-header">
+        <div>
+            <form className='form' onSubmit={handleUpdate}>
+                <p>Enter the contact's details below</p>
+                <label htmlFor="firstName">First Name: </label>
+                <input 
+                type="text" 
+                className="input"
+                placeholder="Enter FirstName" 
+                name="firstName" 
+                onChange={onChangeEmail}
+                autoComplete="on"
+                required/><br />
+                <label htmlFor="lastName">Last Name: </label>
+                <input 
+                type="text" 
+                className="input"
+                placeholder="Enter LastName" 
+                name="lastName" 
+                onChange={onChangeEmail}
+                autoComplete="on"
+                required/><br />
+                <label htmlFor="email">Email: </label>
+                <input 
+                type="text" 
+                className="input"
+                placeholder="Enter Email" 
+                name="email" 
+                onChange={onChangeEmail}
+                autoComplete="on"
+                required/><br />
+                <label htmlFor="phone">Phone: </label>
+                <input 
+                type="text" 
+                className="input"
+                placeholder="Enter Phone" 
+                name="phone" 
+                onChange={onChangeEmail}
+                autoComplete="on"
+                required/><br />
+                <label htmlFor="comments">Comments: </label>
+                <input 
+                type="text" 
+                className="input"
+                placeholder="Enter Comment" 
+                name="comment" 
+                onChange={onChangeEmail}
+                autoComplete="on"
+                required/><br />
+                <input
+                type="submit"
+                className="btn"
+                name="Add Contact"
+                value="Add Contact"
+                autoComplete="on"
+                /> 
+            </form>
+        </div>
    </header>
    );
 }
