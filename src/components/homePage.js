fb9d315af982fb68ae1a903eab805b1e6ffb4e4a
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-10-05 16:05:49 +1100

    Comp-101: feat! Calendar (note event not always displaying)

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,16 +2,30 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import axios from "../services/backendApi.js";
 import HorizontalScroll from "react-scroll-horizontal";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import moment from 'moment';
 import '../App.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const HomePage = () => {
     const [items, setItems] = useState([]);
     const [events, setEvents] = useState([]);
+    const [calendarEvents, setCalendarEvents] = useState([]);
     const GetContacts = () =>{
             axios.get("/contacts").then(res => {setItems(res.data);})
     }
     const GetEvents = () =>{
-        axios.get("/events").then(res => {setEvents(res.data);})
+        axios.get("/events").then(res => {
+        let calendarEvents = [];
+        setEvents(res.data);
+        for (let index in events){
+            let obj = {title: events[index].title, start: events[index].start_time, end: events[index].end_time, url: `/eventDetails/${events[index]._id}`, backgroundColor: events[index].colour}
+            calendarEvents.push(obj);
+        }
+        console.log(calendarEvents);
+        setCalendarEvents(calendarEvents);
+        })
     }
     useEffect(() => {
         GetContacts();
@@ -39,11 +53,14 @@ const HomePage = () => {
             
         <div>             
             <h1> Events </h1>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                events={calendarEvents}
+            />
             {events.map((event, key) => (
                 <Link to={`/eventDetails/${event._id}`}>
                     <div>
                         <p>{event.title} </p>
-                        <p>{event.start} {event.end}</p>
                     </div>
                 </Link>
             ))}
