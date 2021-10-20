1a1af59db64e43926e1204d4e9aa1e28414e5bf8
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-10-20 21:54:12 +1100

    COMP-152: fix! Now display comments on comments page

--- src/components/comments.js
+++ src/components/comments.js
@@ -6,34 +6,25 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 
 const Comments = (props) => {
-/*
+
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
     const GetComments = () =>{
         axios.get(`/comments/${props.match.params.contactID}`).then(res => {
-        let cComments = [];
         setComments(res.data);
-        console.log(comments)
-        for (let index in res.data){
-            let obj = {contactID: res.data[index].contact_id, comment_body: res.data[index].comment_body}
-            cComments.push(obj);
-        }
+        setComment(res.data[0]);
         });
     }
     useEffect(() => {
-        setComments();
-    }, [])*/
-
-
-    const [comments, setComments] = useState([]);
-	const url = `/comments/${props.match.params.contactID}`;
-	useEffect(() => {
-		setComments(props.match.params.contactID);
-		axios.get(url).then(res => {setComments(res.data)})
-    }, [props.match.params.contactID, url])
+        GetComments();
+    }, [])
 	return (
+            
             <div>
-                <h1>{props.match.params.contactID} {comments.contactID} Comments</h1>
-                <p>{comments[0].comment_body}</p>
+                <h1> {comment.contact_id} Comments</h1>
+                {comments.map((item, key) => (
+                    <p>{item.comment_body}</p>
+                ))}
             </div>
 	);
 }
