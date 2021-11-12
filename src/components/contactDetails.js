90aae9ec111b01b4dc72baa595223382f06c9d08
rwhudson119 <rwhudson@student.unimelb.edu.au>
2021-11-12 16:47:20 +1100

    COMP-213 fix! compiling error

--- src/components/contactDetails.js
+++ src/components/contactDetails.js
@@ -140,7 +140,6 @@ const ContactDetails = (props) => {
 			</div>	
 			{//<Button size="large" variant="contained" href = {`/updateContact/${item._id}`} className='btn'> Update Contact</Button>
 			}<Link to={`/updateContact/${item._id}`} className='btn'>Update Contact</Link>
-			</form>			
 			{item.contact_type && (<Button size="large" variant="contained" href = {file} className='btn'> DownLoad File</Button>)}
 			<Link to={`/updateContact/${item._id}`} className='btn'>Update Contact</Link>
 			<button onClick={()=>handleUpdateDel(item._id)} className='btn'>Delete Contact</button>
